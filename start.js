1ca534ad18e837931e963059d3ab7e0dae367e59
LEESEOYOUNG0107 <s92202763@gmail.com>
2025-06-02 23:37:18 +0900

    progressBar, result explain

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 let main = document.querySelector("#intro");
 let qna = document.querySelector("#qna");
+let endPoint = 8; /*질문 갯수 */
 
 function begin(){
     main.style.animation = "fadeOut 1s";
@@ -17,11 +18,18 @@ function begin(){
 
 
 function goNext(qIndex){
+    if(++qIndex == endPoint){
+        goResult();
+    }
+
     let q = document.querySelector('.question');
     q.innerHTML = qnaList[qIndex].question; /*qnaList n번 방에 있는 question 가져오기*/
     for(let i in qnaList[qIndex].choice){ /*qnaList n번 방에 choice i번째에 text 부분 값 가져오기 */
         addAnswer(qnaList[qIndex].choice[i].text, qIndex); 
     }
+
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIndex) + '%';
 }
 
 function addAnswer(answerText, qIndex){
@@ -50,4 +58,8 @@ function addAnswer(answerText, qIndex){
         }, 450) /*버튼이 사라지고 난 다음*/
         
     });
+}
+
+function goResult(){
+    
 }
