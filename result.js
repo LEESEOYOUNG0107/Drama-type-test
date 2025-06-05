cdfa7c01225dbedbd05de8b000777d9e8decc2b6
LEESEOYOUNG0107 <s92202763@gmail.com>
2025-06-05 15:21:55 +0900

    ui 조금 수정

---
+++ result.js
@@ -0,0 +1,38 @@
+function setResult(){
+    let point = calculation();
+     
+    let resultImg = document.createElement('img');
+    let imgDiv = document.querySelector('#resultImg');
+    let imgUrl = 'imgs/' + point + '.png';
+    resultImg.src = imgUrl;
+    imgDiv.appendChild(resultImg);
+
+    let resultName = document.querySelector('#resultName');
+    resultName.innerHTML = infoList[point].name;
+    
+    let resultDesc = document.querySelector('#resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
+}
+
+function goResult() {
+    qna.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+        qna.style.animation = "fadeIn 1s";
+
+        setTimeout(() => {
+            qna.style.display = "none";
+            result.style.display = "block";
+        }, 450)
+    }, 450)
+
+    setResult();
+}
+
+function calculation(){
+    let result = select.indexOf(Math.max(...select)); //최대값의 인덱스 반환
+    return result;
+}
+
+function restart(){
+    begin();
+}
--- start.js
+++ start.js
@@ -26,7 +26,7 @@ function goNext(qIndex) {
         addAnswer(qnaList[qIndex].a[i].text, qIndex, i);
     }
 
-    var status = document.querySelector('.statusBar');
+    var status = document.querySelector('.progressBar');
     status.style.width = (100 / endPoint) * (qIndex) + '%';
 }
 
@@ -69,37 +69,3 @@ function addAnswer(answerText, qIndex, idx) {
     });
 }
 
-function setResult(){
-    let point = calculation();
-     
-    let resultImg = document.createElement('img');
-    let imgDiv = document.querySelector('#resultImg');
-    let imgUrl = 'imgs/' + point + '.png';
-    resultImg.src = imgUrl;
-    imgDiv.appendChild(resultImg);
-
-    let resultName = document.querySelector('#resultName');
-    resultName.innerHTML = infoList[point].name;
-    
-    let resultDesc = document.querySelector('#resultDesc');
-    resultDesc.innerHTML = infoList[point].desc;
-}
-
-function goResult() {
-    qna.style.animation = "fadeOut 1s";
-    setTimeout(() => {
-        qna.style.animation = "fadeIn 1s";
-
-        setTimeout(() => {
-            qna.style.display = "none";
-            result.style.display = "block";
-        }, 450)
-    }, 450)
-
-    setResult();
-}
-
-function calculation(){
-    let result = select.indexOf(Math.max(...select)); //최대값의 인덱스 반환
-    return result;
-}
